bd61c2c9c84779e22b7752eb1abc74410c861110
narathorn09 <narathorn.work@gmail.com>
2024-09-23 01:53:25 +0700

    fix

--- src/axios-config.js
+++ src/axios-config.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // Your backend URL
-  headers: {
-    'Content-Type': 'application/json',  // Automatically added when sending JSON data
-  },
+//   headers: {
+//     'Content-Type': 'application/json',  // Automatically added when sending JSON data
+//   },
   withCredentials: true,  // Enable this if your backend requires credentials like cookies
 });
 
