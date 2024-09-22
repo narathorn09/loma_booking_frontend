e1eb15ee83fee07674f9c63a379486b6666559fb
narathorn09 <narathorn.work@gmail.com>
2024-09-23 01:37:18 +0700

    fix

--- src/axios-config.js
+++ src/axios-config.js
@@ -5,7 +5,7 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // Your backend URL
   headers: {
     'Content-Type': 'application/json',  // You can set the content-type here if needed
-    'Access-Control-Allow-Origin': 'https://loma-booking-frontend.vercel.app',  
+    'Access-Control-Allow-Origin': 'https://www.loma-booking-frontend.vercel.app',  
   },
   withCredentials: true,  // Enable this if your backend requires credentials like cookies
 });
