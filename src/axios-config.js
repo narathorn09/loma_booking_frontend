f68b33a48443bc9347d52e9b9901aa4b07c66590
narathorn09 <narathorn.work@gmail.com>
2024-09-23 01:47:52 +0700

    fix

--- src/axios-config.js
+++ src/axios-config.js
@@ -1,11 +1,9 @@
-// Example of Axios with custom headers
 import axios from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL, // Your backend URL
   headers: {
-    'Content-Type': 'application/json',  // You can set the content-type here if needed
-    'Access-Control-Allow-Origin': 'https://loma-booking-frontend.vercel.app',  
+    'Content-Type': 'application/json',  // Automatically added when sending JSON data
   },
   withCredentials: true,  // Enable this if your backend requires credentials like cookies
 });
